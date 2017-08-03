0c06063cf257b5af0baae734edf3d561a57939ba
TyrWang <tyrwang@kkbox.com>
2017-08-03 11:26:47 +0800

    first push

---
+++ QRCodeReader/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using ZXing;
+
+namespace QRCodeReader
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            IBarcodeReader reader = new BarcodeReader();
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                var barcodeBitmap = new Bitmap(openFileDialog.FileName);
+                var result = reader.Decode(barcodeBitmap);
+                if (result != null)
+                {
+                    decodedResult.Content = result.ToString();
+                }
+            }
+        }
+    }
+}
